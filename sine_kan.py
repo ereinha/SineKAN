6d0d1a377c16b75f259a986721ea56c8379c1062
ereinha <74273926+ereinha@users.noreply.github.com>
2024-07-22 17:33:42 -0500

    Update sine_kan.py to latest version with learnable frequencies

--- sine_kan.py
+++ sine_kan.py
@@ -9,7 +9,7 @@ def forward_step(i_n, grid_size, A, K, C):
     return i_n1
 
 class SineKANLayer(torch.nn.Module):
-    def __init__(self, input_dim, output_dim, device='cuda', grid_size=5, is_first=False, add_bias=True):
+    def __init__(self, input_dim, output_dim, device='cuda', grid_size=5, is_first=False, add_bias=True, norm_freq=True):
         super(SineKANLayer,self).__init__()
         self.grid_size = grid_size
         self.device = device
@@ -23,16 +23,22 @@ class SineKANLayer(torch.nn.Module):
         self.grid_norm_factor = self.grid_norm_factor.reshape(1, 1, grid_size)
             
         if is_first:
-            self.base_fouriercoeffs = torch.nn.Parameter(torch.empty(output_dim, input_dim, 1).normal_(0, .4) / output_dim  / self.grid_norm_factor)
+            self.amplitudes = torch.nn.Parameter(torch.empty(output_dim, input_dim, 1).normal_(0, .4) / output_dim  / self.grid_norm_factor)
         else:
-            self.base_fouriercoeffs = torch.nn.Parameter(torch.empty(output_dim, input_dim, 1).uniform_(-1, 1) / output_dim  / self.grid_norm_factor)
+            self.amplitudes = torch.nn.Parameter(torch.empty(output_dim, input_dim, 1).uniform_(-1, 1) / output_dim  / self.grid_norm_factor)
 
         grid_phase = torch.arange(1, grid_size + 1).reshape(1, 1, 1, grid_size) / (grid_size + 1)
         self.input_phase = torch.linspace(0, math.pi, input_dim).reshape(1, 1, input_dim, 1).to(device)
         phase = grid_phase.to(device) + self.input_phase
-        
+
+        if norm_freq:
+            self.freq = torch.nn.Parameter(torch.arange(1, grid_size + 1).float().reshape(1, 1, 1, grid_size) / (grid_size + 1)**(1 - is_first))
+        else:
+            self.freq = torch.nn.Parameter(torch.arange(1, grid_size + 1).float().reshape(1, 1, 1, grid_size))
+
         for i in range(1, self.grid_size):
             phase = forward_step(phase, i, self.A, self.K, self.C)
+        # self.phase = torch.nn.Parameter(phase)
         self.register_buffer('phase', phase)
         
         if self.add_bias:
@@ -43,8 +49,8 @@ class SineKANLayer(torch.nn.Module):
         output_shape = x_shape[0:-1] + (self.output_dim,)
         x = torch.reshape(x, (-1, self.input_dim))
         x_reshaped = torch.reshape(x, (x.shape[0], 1, x.shape[1], 1))
-        s = torch.sin(x_reshaped + self.phase)
-        y = torch.einsum('ijkl,jkl->ij', s, self.base_fouriercoeffs)
+        s = torch.sin(x_reshaped * self.freq + self.phase)
+        y = torch.einsum('ijkl,jkl->ij', s, self.amplitudes)
         if self.add_bias:
             y += self.bias
         y = torch.reshape(y, output_shape)
@@ -54,8 +60,6 @@ class SineKAN(torch.nn.Module):
     def __init__(
         self,
         layers_hidden: List[int],
-        grid_min: float = -2.,
-        grid_max: float = 2.,
         grid_size: int = 8,
         device: str = 'cuda',
     ) -> None:
